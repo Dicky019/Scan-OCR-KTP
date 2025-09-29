aa1c2cec808c215a49b95721cb25510d083cdfdd
Dicky Darmawan <dicky.darmawan@nusantarabetastudio.com>
2025-09-29 11:22:08 +0900

    Initial Commit

---
+++ Scan OCR KTP/Scan_OCR_KTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scan_OCR_KTPApp.swift
+//  Scan OCR KTP
+//
+//  Created by Dicky Darmawan on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Scan_OCR_KTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scan OCR KTPTests/Scan_OCR_KTPTests.swift
@@ -0,0 +1,16 @@
+//
+//  Scan_OCR_KTPTests.swift
+//  Scan OCR KTPTests
+//
+//  Created by Dicky Darmawan on 29/09/25.
+//
+
+import Testing
+
+struct Scan_OCR_KTPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
